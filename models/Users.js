55879248dad560b30160c5de5962ab0cf9bb1911
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-12-07 20:47:52 -0600

    Working on post routes

--- models/Users.js
+++ models/Users.js
@@ -7,9 +7,9 @@ const bcrypt = require('bcrypt')
 // Set up method to run on instance data (per user) to check password
 // QUESTION: How does it get passed to this? 
 class User extends Model {
-    // checkPassword(loginPw) {
-    //     return bcrypt.compareSync(loginPw, this.password)
-    // }
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password)
+    }
 }
 
 User.init(
@@ -23,7 +23,7 @@ User.init(
         email: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true,
+            unique: false,
             validate: {
                 isEmail: true
             }
@@ -31,7 +31,7 @@ User.init(
         username: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true
+            unique: false
         },
         password: {
             type: DataTypes.STRING, 
@@ -40,12 +40,12 @@ User.init(
         },
     },
     {
-        // hooks: {
-        //     async beforeCreate(newUserData) {
-        //         newUserData.password = await bcrypt.hash(newUserData.password, 10)
-        //         return newUserData
-        //     }
-        // },
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                return newUserData
+            }
+        },
         sequelize,
         timestamps: true,
         freezeTableName: true,
